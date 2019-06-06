aaa93125f81bad449b9b6abf5f01d855ee992ac3
prasenjit-net <github@prasenjit.net>
2019-06-06 22:06:35 +0530

    Initial Commit

---
+++ src/main/java/net/prasenjit/poc/springsecuritymfa/SpringSecurityMfaApplication.java
@@ -0,0 +1,13 @@
+package net.prasenjit.poc.springsecuritymfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/prasenjit/poc/springsecuritymfa/SpringSecurityMfaApplicationTests.java
@@ -0,0 +1,16 @@
+package net.prasenjit.poc.springsecuritymfa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSecurityMfaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
